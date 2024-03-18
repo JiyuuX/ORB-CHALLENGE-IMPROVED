3751ae1c3ae1d3a20f6fc0a255a7810cd5ec1265
JiyuuX <139239394+JiyuuX@users.noreply.github.com>
2024-03-18 04:19:03 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ourpathtomoon@gmail.com'
-EMAIL_HOST_PASSWORD = 'hvshuszxdfvlzfaa'
+EMAIL_HOST_USER = 'YOUR_EMAIL_ADDRESS'
+EMAIL_HOST_PASSWORD = 'EMAIL_PASSWORD'
